6342b2601d35338818d54c8b85b5822fa36c3d3e
RohanBera <berarohan88@gmail.com>
2021-06-06 17:36:27 +0530

    Profile page one side kinda done

--- src/components/projectsComponents/Projects.js
+++ src/components/projectsComponents/Projects.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 export default function Projects() {
     return (
-        <div>
-            Projects
+        <div className='projects dots'>
+            <div className='left'> brrr</div>
+            <div className='right'>
+                <div className='container'>
+                    <div className='info'>
+                        <div className='heading' >
+                            Projects
+                        </div>
+                        <div className='body' >
+                            Projects. Projects. Projects.
+                        </div>
+                        <div className='matter' >
+                            Lorem ipsum dolor sit amet, <br />consectetur adipiscing.
+                        </div>
+                    </div>
+                    <br />
+                    <br />
+
+                    <a href='https://github.com/RohanBera' className=''>
+                        <div>Aao kabhi <span>Github</span> pe <span role="img" aria-labelledby="heart">🖤</span> </div>
+                        <GitHubIcon style={{ fontSize: 40 }} />
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
