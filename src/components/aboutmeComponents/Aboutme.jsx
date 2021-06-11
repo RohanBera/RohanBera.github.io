1413ca8ae2229b805db4ad693c26fd1597979486
RohanBera <berarohan88@gmail.com>
2021-06-11 21:05:21 +0530

    ABOUT ME DONE?

--- src/components/aboutmeComponents/Aboutme.jsx
+++ src/components/aboutmeComponents/Aboutme.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
+import Typewriter from 'typewriter-effect';
+
+import dio from '../../assets/images/aboutme.gif'
 
 export default function Aboutme() {
     return (
-        <div className='home' id='about'>
-            <div className="left image-align"></div>
-            <div className="right image-align"></div>
+        <div className='home about' id='about'>
+            <div className="left">
+                <div className="about-gif">
+                    <img src={dio} alt="dio" />
+                </div>
+                <div className="about-heading">
+                    <Typewriter
+                        options={{
+                            deleteSpeed: 15,
+                            strings: ['Human rohan = new Human();'],
+                            autoStart: true,
+                            loop: true,
+                        }}
+                    />
+                </div>
+            </div>
+            <div className="right">
+                <div className="about-info">
+                    <div className="heading-info">
+                        about me.
+                    </div>
+                    <div className="main-info">
+                        I am a student at University of Hyderabad pursuing Integrated Mtech in Computer Science.
+                    </div>
+                    <div className="sub-info">
+                        <p>Sunt aliqua sunt ea nostrud qui duis.Commodo aute aliquip fugiat ea tempor non ea aliquip occaecat.Fugiat mollit exercitation consectetur mollit sint aute velit eiusmod nisi pariatur officia.Lorem eu amet magna veniam. Non aute commodo deserunt fugiat aute magna ipsum in. Nostrud fugiat laborum anim est dolor consequat. Lorem ut pariatur fugiat nostrud nulla aliqua cillum culpa exercitation eiusmod pariatur anim. Aliqua ullamco deserunt laborum nulla consectetur culpa adipisicing id nostrud reprehenderit et non. Incididunt non excepteur consequat ipsum pariatur velit fugiat tempor velit. Elit enim ad esse elit qui.</p>
+                        <p>When I am not coding, you'll find me at the gym or in my room with my guitar.</p>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
