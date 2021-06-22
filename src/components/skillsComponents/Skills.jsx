33527a90b52a4cd3abb2781c78f8acee935457a4
RohanBera <berarohan88@gmail.com>
2021-06-22 15:09:15 +0530

    SKILLS

--- src/components/skillsComponents/Skills.jsx
+++ src/components/skillsComponents/Skills.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
 
+import { skillsList } from './skillsList'
+
 export default function Skills() {
     return (
         <div className='home skills' id='skills'>
             <div className="left">
                 <div className="heading">skills</div>
-                <div className="skills-info">
-                    <div className="heading-info"></div>
-                    <div className="stack"></div>
+                <div className="skills-info-container">
+
+                    {skillsList.map((item, index) => {
+                        return (
+                            <div className="skills-info" key={index}>
+                                <div className="heading-info">{item.title}</div>
+                                <div className="stack">
+                                    {item.stack.map((stItem, stIndex) => {
+
+                                        let tooltiptext = stItem.split('/')[3].split('.')[0]
+                                        return (
+                                            <div key={stIndex} className="tooltip">
+                                                <img src={stItem} alt={'logo ' + stIndex} />
+                                                <div className="tooltiptext">{tooltiptext}</div>
+                                            </div>
+                                        );
+
+                                    })}
+                                </div>
+                            </div>
+                        )
+                    })}
+
                 </div>
             </div>
             <div className="right image-align rohan"></div>
