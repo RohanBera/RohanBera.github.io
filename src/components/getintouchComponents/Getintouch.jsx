c02a91196fc677ceff3ff62750130d4bb0fd9320
RohanBera <berarohan88@gmail.com>
2021-06-22 14:57:02 +0530

    GET IN TOUCH

--- src/components/getintouchComponents/Getintouch.jsx
+++ src/components/getintouchComponents/Getintouch.jsx
@@ -1,26 +1,40 @@
 import React from 'react'
+import { getInTouchList } from './getintouchList'
 
 export default function Getintouch() {
     return (
         <div className="home contact" id="contact">
             <div className="left">
                 <div className="contact-heading">
-
                     get in touch
                 </div>
-                <br />
-                mail
-                <br />
-                Github
-                <br />
-                Linkedin
-                <br />
-                instagram
-            </div>
 
-            <div className="right image-align duck">
+                <div className="contact-body">
+                    {
+                        getInTouchList.map((item, index) => {
+                            return (
+                                <SocialLink
+                                    key={index}
+                                    Icon={item.icon}
+                                    text={item.text}
+                                    linkTo={item.linkTo}
+                                    className={item.class}
+                                />
+                            )
+                        })
+                    }
+                </div>
 
             </div>
+            <div className="right image-align duck"></div>
         </div>
+    )
+}
+
+const SocialLink = ({ text, Icon, linkTo, className }) => {
+    return (
+        <a title={text} className={`contact ${className}`} href={linkTo}>
+            <Icon className='large-icon' fontSize='large' />
+        </a>
     )
 }
---
+++ src/components/getintouchComponents/getintouchList.js
@@ -0,0 +1,31 @@
+import MailIcon from '@material-ui/icons/Mail';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+
+export const getInTouchList = [
+    {
+        text: "Mail",
+        class: "mail",
+        icon: MailIcon,
+        linkTo: 'mailto:17mcme21@uohyd.ac.in',
+    },
+    {
+        text: "Github",
+        class: "github",
+        icon: GitHubIcon,
+        linkTo: 'https://github.com/RohanBera',
+    },
+    {
+        text: "LinkedIn",
+        class: "linkedin",
+        icon: LinkedInIcon,
+        linkTo: 'https://www.linkedin.com/in/rohanbera/',
+    },
+    {
+        text: "Instagram",
+        class: "instagram",
+        icon: InstagramIcon,
+        linkTo: 'https://www.instagram.com/rohan__ruku/',
+    }
+]
