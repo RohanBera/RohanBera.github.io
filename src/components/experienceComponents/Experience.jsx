da1e8fdfd0494677b3bf9abd1506703af6981400
RohanBera <berarohan88@gmail.com>
2021-06-23 17:50:49 +0530

    EXPERIENCE (mater remaining)

--- src/components/experienceComponents/Experience.jsx
+++ src/components/experienceComponents/Experience.jsx
@@ -2,14 +2,51 @@ import React from 'react'
 
 export default function Experience() {
     return (
-        <div class="home experience" id="experience">
-            <div className="left">
-
+        <div className="experience" id="experience">
+            <div className="exp-heading">
+                experience
             </div>
 
-            <div className="right">
-                Experience
+            <div className="exp-cards-container">
+                <div className="exp-card">
+                    <div className="job-title">
+                        SDE Intern
+                    </div>
+                    <div>
+                        at <span className="organization">Apty</span>
+                    </div>
+                    <div className="duration">
+                        12th oct to 14th nov 2050
+                    </div>
+                    <div className="card-info">
+                        <ul>
+                            <li>Built an admin page to manage onboarding new users</li>
+                            <li>Built an e-commerce website, which was used as a testing ground for their product.</li>
+                            <li>Learnt ReactJS</li>
+                        </ul>
+                        <a href="https://github.com/RohanBera/Interpolation-using-PYDIVA">Read more...</a>
+                    </div>
+                </div>
 
+                <div className="exp-card">
+                    <div className="job-title">
+                        SDE Intern
+                    </div>
+                    <div>
+                        at <span className="organization">Apty</span>
+                    </div>
+                    <div className="duration">
+                        12th oct to 14th nov 2050
+                    </div>
+                    <div className="card-info">
+                        <ul>
+                            <li>Built an admin page to manage onboarding new users</li>
+                            <li>Built an e-commerce website, which was used as a testing ground for their product.</li>
+                            <li>Learnt ReactJS</li>
+                        </ul>
+                        <a href="https://github.com/RohanBera/Interpolation-using-PYDIVA">Read more...</a>
+                    </div>
+                </div>
             </div>
         </div>
     )
