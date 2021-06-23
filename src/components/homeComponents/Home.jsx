c8f67584d90013f4703df77287148d7de73033d2
RohanBera <berarohan88@gmail.com>
2021-06-23 17:50:06 +0530

    Sound added to namaste

--- src/components/homeComponents/Home.jsx
+++ src/components/homeComponents/Home.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
+import useSound from 'use-sound'
+
+function importAll(r) {
+    let notes = {};
+    r.keys().forEach((item, index) => {
+        notes[item.replace('./', '')] = r(item);
+    });
+    return notes;
+}
 
 export default function Home() {
+    const notes = importAll(require.context('../../assets/sounds', false, /\.(wav)$/));
+
     return (
         <div className="home">
             <div className="left">
                 <div className="info">
                     <div className="heading">
-                        <span className="namaste">Namaste</span> World.
-                </div>
+                        {
+                            'namaste'.split('').map((letter, index) => {
+                                return (
+                                    <SingleLetter
+                                        key={index}
+                                        letter={letter}
+                                        zIndex={8 - index}
+                                        note={notes['note' + (index + 1) + '.wav']}
+                                    />
+                                )
+                            })
+                        }
+                        {' World.'}
+                    </div>
                     <div className="sub-heading">
                         I am Rohan Bera. <br />
                         lorem ipsum dolor sit.
@@ -16,5 +39,21 @@ export default function Home() {
             </div>
             <div className="right image-align rohan"></div>
         </div>
+    )
+}
+
+function SingleLetter({ letter, note, zIndex }) {
+
+    const [play] = useSound(
+        note, { volume: 0.3 }
+    )
+
+    return (
+        <span
+            onMouseEnter={() => { play() }}
+            style={{ zIndex: zIndex }}
+            className="namaste">
+            {letter}
+        </span>
     )
 }
